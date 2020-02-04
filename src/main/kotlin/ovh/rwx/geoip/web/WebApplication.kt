80f37680e3d125ca55d09f104d707f8b28ea352d
José Olívio M. Pedrosa <root@rwx.ovh>
2020-02-04 11:17:08 -0300

    Fuck MaxMind's blocking free download without needing an account

--- src/main/kotlin/ovh/rwx/geoip/web/WebApplication.kt
+++ src/main/kotlin/ovh/rwx/geoip/web/WebApplication.kt
@@ -1,20 +1,10 @@
 package ovh.rwx.geoip.web
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.scheduling.annotation.EnableScheduling
-import ovh.rwx.geoip.web.tasks.GeoIpDatabase
 
 @SpringBootApplication
-@EnableScheduling
-class WebApplication {
-    @Bean
-    fun init(geoIpDatabase: GeoIpDatabase) = CommandLineRunner {
-        geoIpDatabase.updateGeoIpDatabase()
-    }
-}
+class WebApplication
 
 fun main(args: Array<String>) {
     runApplication<WebApplication>(*args)
--- src/main/kotlin/ovh/rwx/geoip/web/services/impl/GeoIpServiceImpl.kt
+++ src/main/kotlin/ovh/rwx/geoip/web/services/impl/GeoIpServiceImpl.kt
@@ -1,24 +1,22 @@
 package ovh.rwx.geoip.web.services.impl
 
+import com.maxmind.db.CHMCache
 import com.maxmind.geoip2.DatabaseReader
 import org.slf4j.LoggerFactory
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Service
 import ovh.rwx.geoip.web.objects.GeoIp
 import ovh.rwx.geoip.web.services.GeoIpService
-import ovh.rwx.geoip.web.tasks.GeoIpDatabase
 import java.net.InetAddress
 
 @Service
-internal class GeoIpServiceImpl(
-        private val geoIpDatabase: GeoIpDatabase
-) : GeoIpService {
+internal class GeoIpServiceImpl : GeoIpService {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    override fun searchIp(inetAddress: InetAddress): GeoIp {
-        val databaseReaderCity: DatabaseReader = DatabaseReader.Builder(geoIpDatabase.GEOIP_CITY_DB_FILE).build()
-        val databaseReaderASN: DatabaseReader = DatabaseReader.Builder(geoIpDatabase.GEOIP_ASN_DB_FILE).build()
+    private val databaseReaderCity: DatabaseReader by lazy { DatabaseReader.Builder(ClassPathResource("geoip/GeoLite2-City.mmdb").file).withCache(CHMCache()).build() }
+    private val databaseReaderASN: DatabaseReader by lazy { DatabaseReader.Builder(ClassPathResource("geoip/GeoLite2-ASN.mmdb").file).withCache(CHMCache()).build() }
 
+    override fun searchIp(inetAddress: InetAddress): GeoIp {
         val city = try {
             databaseReaderCity.city(inetAddress)
         } catch (e: Exception) {
