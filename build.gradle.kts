34938c817d64604f6a2fca43fbe97cb082fe3a84
José Olívio M. Pedrosa <root@rwx.ovh>
2020-02-26 10:16:51 -0300

    [CI SKIP] Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-    id("org.springframework.boot") version "2.2.0.RELEASE"
-    id("io.spring.dependency-management") version "1.0.8.RELEASE"
-    kotlin("jvm") version "1.3.50"
-    kotlin("plugin.spring") version "1.3.50"
+    id("org.springframework.boot") version "2.2.4.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    kotlin("jvm") version "1.3.61"
+    kotlin("plugin.spring") version "1.3.61"
 }
 
 group = "ovh.rwx.geoip"
@@ -37,9 +37,6 @@ dependencies {
     }
     compile("com.maxmind.geoip2:geoip2:2.12.0")
     compile("dnsjava:dnsjava:2.1.9")
-    compile("net.java.truevfs:truevfs-access:0.12.2")
-    compile("net.java.truevfs:truevfs-kernel-impl_2.13:0.12.2")
-    compile("net.java.truevfs:truevfs-driver-tar-gzip:0.12.2")
 }
 
 tasks.withType<Test> {
